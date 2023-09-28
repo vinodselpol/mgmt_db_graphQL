191f190b5c8d2bb39335064fef6f85da17489165
vinodselpol <vinodselpol@example.com>
2023-09-28 12:28:47 -0500

    Client side rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,55 @@
 
 import Header from "./components/Header";
 import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client'
-import Clients from "./components/Clients";
+
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Project from "./pages/Project";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
 
 const client = new ApolloClient({
   uri: 'http://localhost:8000/graphql',
-  cache: new InMemoryCache()
+  cache,
 })
 function App() {
 
   return (
     <>
     <ApolloProvider client = {client} >
+    <Router>
     <Header />
      <div className="container">
-      <Clients />
+     <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects/:id' element={<Project />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     
   
     </div>
+
+    </Router>
+    
+   
     </ApolloProvider>
     </>
    
--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,21 +1,14 @@
-import {gql, useQuery} from '@apollo/client'
+import { useQuery} from '@apollo/client'
 import ClientRow from './ClientRow';
+import { GET_CLIENTS } from '../queries/clientQueries';
 
-const GET_CLIENTS = gql`
-query getClients {
-  clients {
-    id
-    name
-    email
-    phone
-  }
-}
-`;
+import Spinner from './Spinner';
+
 
 function Clients() {
     const {loading, error, data} = useQuery(GET_CLIENTS);
 
-    if (loading) return <p>Loading.....</p>
+    if (loading) return (<Spinner />)
     if (error) return <p>Something went Wrong</p>
 
 
